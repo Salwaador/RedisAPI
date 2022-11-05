3eec7de53ebf7448953760f1b40c5b1c84bd68cd
Radosław Łuczak <radoslaw.luczak.dev@gmail.com>
2022-11-05 13:55:31 +0100

    Add project files.

---
+++ RedisAPI/Controllers/PlatformsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisAPI.Data;
+using RedisAPI.Models;
+
+namespace RedisAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _platformRepo;
+
+        public PlatformsController(IPlatformRepo platformRepo)
+        {
+            _platformRepo = platformRepo;
+        }
+
+        [HttpGet("{id}", Name = "GetPlatform")]  
+        public ActionResult<Platform> GetPlatform(string id)
+        {
+            var platform = _platformRepo.GetPlatformById(id);
+
+            if (platform is null)
+            {
+                return NotFound($"Platform with id: '{id}' does not exist.");
+            }
+
+            return Ok(platform);
+        }
+
+        [HttpGet]
+        public ActionResult GetPlatforms()
+        {
+            var platforms = _platformRepo.GetAllPlatforms();                      
+
+            return Ok(platforms);
+        }
+
+        [HttpPost]
+        public ActionResult<Platform> CreatePlatform(Platform platform)
+        {
+            _platformRepo.CreatePlatform(platform);
+
+            return CreatedAtRoute(nameof(GetPlatform), new { Id = platform.Id}, platform);              
+        }
+    }
+}
---
+++ RedisAPI/Data/RedisPlatformRepo.cs
@@ -0,0 +1,69 @@
+﻿using RedisAPI.Models;
+using StackExchange.Redis;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace RedisAPI.Data
+{
+    public class RedisPlatformRepo : IPlatformRepo
+    {
+        private readonly IConnectionMultiplexer _connectionMultiplexer;
+
+        public RedisPlatformRepo(IConnectionMultiplexer connectionMultiplexer)
+        {
+            _connectionMultiplexer = connectionMultiplexer;
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            if (platform is null)
+            {
+                throw new ArgumentException(nameof(platform));
+            }
+            
+            var serializePlatform = JsonSerializer.Serialize(platform);
+
+            var db = _connectionMultiplexer.GetDatabase();
+
+            //db.StringSet(platform.Id, serializePlatform);
+            //db.SetAdd("PlatformSet", serializePlatform);
+            //
+
+            db.HashSet("hashplatform", new HashEntry[]{ new HashEntry(platform.Id, serializePlatform)});
+        }
+
+        public IEnumerable<Platform?>? GetAllPlatforms()
+        {
+            var db = _connectionMultiplexer.GetDatabase();
+
+            // var completeSet = db.SetMembers("PlatformSet");
+
+            var completeHash = db.HashGetAll("hashplatform");
+
+            if (completeHash.Length > 0)
+            {
+                var platforms = Array.ConvertAll(completeHash, x => JsonSerializer.Deserialize<Platform>(x.Value)).ToList();
+
+                return platforms;
+            }
+
+            return null;
+        }
+
+        public Platform? GetPlatformById(string id)
+        {
+            var db = _connectionMultiplexer.GetDatabase();
+            //var serializePlatform = db.StringGet(id);
+
+            var serializePlatform = db.HashGet("hashplatform", id);
+
+            if (!string.IsNullOrEmpty(serializePlatform))
+            {
+                var platform = JsonSerializer.Deserialize<Platform>(serializePlatform);
+                return platform;
+            }
+
+            return null;
+        }
+    }
+}
